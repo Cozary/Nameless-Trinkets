3962fe40dfce1bb174d7ccefa90c34443c7f2f94
Cozary <ayrecio99@gmail.com>
2025-03-05 13:51:13 +0100

    [True Heart Of The Sea] Not working properly.

--- common/src/main/java/com/cozary/nameless_trinkets/items/trinkets/TrueHeartOfTheSea.java
+++ common/src/main/java/com/cozary/nameless_trinkets/items/trinkets/TrueHeartOfTheSea.java
@@ -50,7 +50,9 @@ public class TrueHeartOfTheSea extends TrinketItem<TrueHeartOfTheSea.Stats> {
     }
 
     @Override
-    public void onEquip(ItemStack stack, SlotReference reference) {
+    public void tick(ItemStack stack, SlotReference reference) {
+        super.tick(stack, reference);
+
         LivingEntity livingEntity = reference.entity();
         Stats config = TrueHeartOfTheSea.INSTANCE.getTrinketConfig();
         if (!config.isEnable)
@@ -76,6 +78,14 @@ public class TrueHeartOfTheSea extends TrinketItem<TrueHeartOfTheSea.Stats> {
         } else {
             livingEntity.setAirSupply(300);
         }
+    }
+
+    @Override
+    public void onEquip(ItemStack stack, SlotReference reference) {
+        LivingEntity livingEntity = reference.entity();
+        Stats config = TrueHeartOfTheSea.INSTANCE.getTrinketConfig();
+        if (!config.isEnable)
+            return;
 
         Level world = livingEntity.getCommandSenderWorld();
 
@@ -89,6 +99,7 @@ public class TrueHeartOfTheSea extends TrinketItem<TrueHeartOfTheSea.Stats> {
         assert attribSpeed != null;
         EntityUtils.applyAttributeModifier(attribSpeed, speedModifier);
     }
+
 
     @Override
     public void onUnequip(ItemStack stack, SlotReference reference) {
