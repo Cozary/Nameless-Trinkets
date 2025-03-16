b5caa6bc6bcf01be7be14f7ae79cf6893cffaa1a
Cozary <ayrecio99@gmail.com>
2025-03-16 23:01:33 +0100

    Added [Dying Star] trinket

--- common/src/main/java/com/cozary/nameless_trinkets/init/ModDataComponents.java
+++ common/src/main/java/com/cozary/nameless_trinkets/init/ModDataComponents.java
@@ -37,6 +37,139 @@ public class ModDataComponents {
                     .build()
     );
 
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_ARMOR = DATA_COMPONENTS.register("dying_star_armor",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_ARMOR_TOUGHNESS = DATA_COMPONENTS.register("dying_star_armor_toughness",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_ATTACK_DAMAGE = DATA_COMPONENTS.register("dying_star_attack_damage",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_ATTACK_KNOCKBACK = DATA_COMPONENTS.register("dying_star_attack_knockback",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_ATTACK_SPEED = DATA_COMPONENTS.register("dying_star_attack_speed",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_BLOCK_BREAK_SPEED = DATA_COMPONENTS.register("dying_star_block_break_speed",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_BLOCK_INTERACTION_RANGE = DATA_COMPONENTS.register("dying_star_block_interaction_range",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_EXPLOSION_KNOCKBACK_RESISTANCE = DATA_COMPONENTS.register("dying_star_explosion_knockback_resistance",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_ENTITY_INTERACTION_RANGE = DATA_COMPONENTS.register("dying_star_entity_interaction_range",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_FALL_DAMAGE_MULTIPLIER = DATA_COMPONENTS.register("dying_star_fall_damage_multiplier",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_FLYING_SPEED = DATA_COMPONENTS.register("dying_star_flying_speed",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_KNOCKBACK_RESISTANCE = DATA_COMPONENTS.register("dying_star_knockback_resistance",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_LUCK = DATA_COMPONENTS.register("dying_star_luck",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_MAX_ABSORPTION = DATA_COMPONENTS.register("dying_star_max_absorption",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_MAX_HEALTH = DATA_COMPONENTS.register("dying_star_max_health",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_MINING_EFFICIENCY = DATA_COMPONENTS.register("dying_star_mining_efficiency",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_MOVEMENT_SPEED = DATA_COMPONENTS.register("dying_star_movement_speed",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_OXYGEN_BONUS = DATA_COMPONENTS.register("dying_star_oxygen_bonus",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_SNEAKING_SPEED = DATA_COMPONENTS.register("dying_star_sneaking_speed",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_SUBMERGED_MINING_SPEED = DATA_COMPONENTS.register("dying_star_submerged_mining_speed",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_SWEEPING_DAMAGE_RATIO = DATA_COMPONENTS.register("dying_star_sweeping_damage_ratio",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+    public static final RegistryObject<DataComponentType<Float>> DYING_STAR_WATER_MOVEMENT_EFFICIENCY = DATA_COMPONENTS.register("dying_star_water_movement_efficiency",
+            () -> DataComponentType.<Float>builder()
+                    .persistent(Codec.FLOAT)
+                    .build()
+    );
+
+
 
     public static final RegistryObject<DataComponentType<TrinketBundleContents>> TRINKET_BUNDLE_CONTENTS = DATA_COMPONENTS.register("trinket_bundle_contents",
             () -> {
--- common/src/main/java/com/cozary/nameless_trinkets/init/ModItems.java
+++ common/src/main/java/com/cozary/nameless_trinkets/init/ModItems.java
@@ -71,6 +71,7 @@ public class ModItems {
     public static final RegistryObject<Item> SHRINKING_VEIL = registerWithTab("shrinking_veil", ShrinkingVeil::new);
     public static final RegistryObject<Item> TITANS_MARK = registerWithTab("titans_mark", TitansMark::new);
     public static final RegistryObject<Item> WOUNDBEARER = registerWithTab("woundbearer", Woundbearer::new);
+    public static final RegistryObject<Item> DYINGSTAR = registerWithTab("dying_star", DyingStar::new);
 
     public static final RegistryObject<Item> TRINKET_BUNDLE = registerWithTab("trinket_bundle", TrinketBundle::new);
 
