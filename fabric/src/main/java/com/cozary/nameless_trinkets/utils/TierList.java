63c91a6848f52825f6a7a99447effa5c6288b3ab
Cozary <ayrecio99@gmail.com>
2025-03-17 00:34:46 +0100

    [Fabric] loot table logic moved

---
+++ fabric/src/main/java/com/cozary/nameless_trinkets/utils/TierList.java
@@ -0,0 +1,60 @@
+package com.cozary.nameless_trinkets.utils;
+
+import com.cozary.nameless_trinkets.init.ModItems;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TierList {
+
+    public static ArrayList<ArrayList<Object>> trinketList = new ArrayList<>(Arrays.asList(
+            new ArrayList<>(Arrays.asList(0.01, ModItems.GODS_CROWN.get())),
+            new ArrayList<>(Arrays.asList(0.025, ModItems.REVERSE_CARD.get())),
+            new ArrayList<>(Arrays.asList(0.025, ModItems.BROKEN_ANKH.get())),
+            new ArrayList<>(Arrays.asList(0.025, ModItems.RAGE_MIND.get())),
+            new ArrayList<>(Arrays.asList(0.025, ModItems.CRACKED_CROWN.get())),
+            new ArrayList<>(Arrays.asList(0.025, ModItems.SIGIL_OF_BAPHOMET.get())),
+            new ArrayList<>(Arrays.asList(0.025, ModItems.TRUE_HEART_OF_THE_SEA.get())),
+            new ArrayList<>(Arrays.asList(0.025, ModItems.REFORGER.get())),
+            new ArrayList<>(Arrays.asList(0.025, ModItems.FATE_EMERALD.get())),
+            new ArrayList<>(Arrays.asList(0.025, ModItems.LIGHT_GLOVES.get())),
+            new ArrayList<>(Arrays.asList(0.025, ModItems.FOUR_LEAF_CLOVER.get())),
+            new ArrayList<>(Arrays.asList(0.025, ModItems.MINERS_SOUL.get())),
+            new ArrayList<>(Arrays.asList(0.05, ModItems.MISSING_PAGE.get())),
+            new ArrayList<>(Arrays.asList(0.05, ModItems.EXPERIENCE_BATTERY.get())),
+            new ArrayList<>(Arrays.asList(0.05, ModItems.SUPER_MAGNET.get())),
+            new ArrayList<>(Arrays.asList(0.05, ModItems.WHAT_MAGNET.get())),
+            new ArrayList<>(Arrays.asList(0.05, ModItems.CALLUS.get())),
+            new ArrayList<>(Arrays.asList(0.05, ModItems.SPEED_FORCE.get())),
+            new ArrayList<>(Arrays.asList(0.05, ModItems.VAMPIRE_BLOOD.get())),
+            new ArrayList<>(Arrays.asList(0.05, ModItems.TICK.get())),
+            new ArrayList<>(Arrays.asList(0.05, ModItems.GHAST_EYE.get())),
+            new ArrayList<>(Arrays.asList(0.05, ModItems.FERTILIZER.get())),
+            new ArrayList<>(Arrays.asList(0.05, ModItems.TEAR_OF_THE_SEA.get())),
+            new ArrayList<>(Arrays.asList(0.05, ModItems.ETHEREAL_WINGS.get())),
+            new ArrayList<>(Arrays.asList(0.05, ModItems.ELECTRIC_PADDLE.get())),
+            new ArrayList<>(Arrays.asList(0.05, ModItems.FRACTURED_NULLSTONE.get())),
+            new ArrayList<>(Arrays.asList(0.05, ModItems.FRAGILE_CLOUD.get())),
+            new ArrayList<>(Arrays.asList(0.05, ModItems.SCARAB_AMULET.get())),
+            new ArrayList<>(Arrays.asList(0.1, ModItems.EXPERIENCE_MAGNET.get())),
+            new ArrayList<>(Arrays.asList(0.1, ModItems.BROKEN_MAGNET.get())),
+            new ArrayList<>(Arrays.asList(0.1, ModItems.LUCKY_ROCK.get())),
+            new ArrayList<>(Arrays.asList(0.1, ModItems.PUFFER_FISH_LIVER.get())),
+            new ArrayList<>(Arrays.asList(0.1, ModItems.BLINDFOLD.get())),
+            new ArrayList<>(Arrays.asList(0.1, ModItems.EXPLOSION_PROOF_JACKET.get())),
+            new ArrayList<>(Arrays.asList(0.1, ModItems.WOODEN_STICK.get())),
+            new ArrayList<>(Arrays.asList(0.1, ModItems.BLAZE_NUCLEUS.get())),
+            new ArrayList<>(Arrays.asList(0.1, ModItems.ICE_CUBE.get())),
+            new ArrayList<>(Arrays.asList(0.1, ModItems.CREEPER_SENSE.get())),
+            new ArrayList<>(Arrays.asList(0.1, ModItems.AMPHIBIOUS_HANDS.get())),
+            new ArrayList<>(Arrays.asList(0.1, ModItems.GILLS.get())),
+            new ArrayList<>(Arrays.asList(0.1, ModItems.MOON_STONE.get())),
+            new ArrayList<>(Arrays.asList(0.1, ModItems.SLEEPING_PILLS.get())),
+            new ArrayList<>(Arrays.asList(0.1, ModItems.NELUMBO.get())),
+            new ArrayList<>(Arrays.asList(0.1, ModItems.DARK_NELUMBO.get())),
+            new ArrayList<>(Arrays.asList(0.1, ModItems.SPIDER_LEGS.get())),
+            new ArrayList<>(Arrays.asList(0.1, ModItems.DRAGONS_EYE.get())),
+            new ArrayList<>(Arrays.asList(0.1, ModItems.POCKET_LIGHTNING_ROD.get()))
+    ));
+
+}
