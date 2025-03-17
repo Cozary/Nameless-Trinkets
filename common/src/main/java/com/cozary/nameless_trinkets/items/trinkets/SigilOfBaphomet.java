0f07c52720d6103c6d6dcc9c1144f4d546d99cb0
Cozary <ayrecio99@gmail.com>
2025-03-17 21:11:30 +0100

    [Sigil Of Baphomet] added config to modify added time and max time. Fixed added time and maximum being much lower than it should be.

--- common/src/main/java/com/cozary/nameless_trinkets/items/trinkets/SigilOfBaphomet.java
+++ common/src/main/java/com/cozary/nameless_trinkets/items/trinkets/SigilOfBaphomet.java
@@ -59,9 +59,9 @@ public class SigilOfBaphomet extends TrinketItem<SigilOfBaphomet.Stats> {
         } else {
             tooltip.add(Component.translatable("tooltip.nameless_trinkets.sigil_of_baphomet_lore").withStyle(ChatFormatting.AQUA, ChatFormatting.ITALIC));
             if (Screen.hasShiftDown()) {
-                tooltip.add(Component.translatable("tooltip.nameless_trinkets.sigil_of_baphomet_1").withStyle(ChatFormatting.GOLD));
+                tooltip.add(Component.translatable("tooltip.nameless_trinkets.sigil_of_baphomet_1", config.invulnerabilityAddTimeInTicks/20).withStyle(ChatFormatting.GOLD));
                 tooltip.add(Component.translatable("tooltip.nameless_trinkets.sigil_of_baphomet_2").withStyle(ChatFormatting.GOLD));
-                tooltip.add(Component.translatable("tooltip.nameless_trinkets.sigil_of_baphomet_3").withStyle(ChatFormatting.GOLD));
+                tooltip.add(Component.translatable("tooltip.nameless_trinkets.sigil_of_baphomet_3", config.invulnerabilityMaxTimeInTicks/20).withStyle(ChatFormatting.GOLD));
             } else {
                 tooltip.add(Component.translatable("tooltip.nameless_trinkets.hold_shift"));
             }
@@ -70,6 +70,9 @@ public class SigilOfBaphomet extends TrinketItem<SigilOfBaphomet.Stats> {
 
     public static class Stats extends TrinketsStats {
         public float fabricLootTableChance = 0.0025f;
+        public int invulnerabilityAddTimeInTicks = 20;
+        public int invulnerabilityMaxTimeInTicks = 200;
+
         public boolean isEnable = true;
 
     }
