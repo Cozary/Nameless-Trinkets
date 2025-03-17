876fe28815d6a88e634494ad43d4fd429be28b7f
Cozary <ayrecio99@gmail.com>
2025-03-17 20:08:39 +0100

    [Ghast Eye] nerf

--- common/src/main/java/com/cozary/nameless_trinkets/items/trinkets/GhastEye.java
+++ common/src/main/java/com/cozary/nameless_trinkets/items/trinkets/GhastEye.java
@@ -92,9 +92,9 @@ public class GhastEye extends TrinketItem<GhastEye.Stats> {
 
     public static class Stats extends TrinketsStats {
         public float fabricLootTableChance = 0.005f;
-        public float extraHearts = 20.0F;
-        public int regenerationTime = 200;
-        public int regenerationExtraTime = 60;
+        public float extraHearts = 10.0F;
+        public int regenerationTime = 150;
+        public int regenerationExtraTime = 40;
         public int regenerationLevel = 0;
         public boolean isEnable = true;
 
