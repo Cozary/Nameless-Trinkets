3d7353da1fac8c30fe52ac4d3e7838315644ec59
Cozary <ayrecio99@gmail.com>
2025-03-19 17:24:56 +0100

    [Fertilizer] Added new description

--- common/src/main/java/com/cozary/nameless_trinkets/items/trinkets/Fertilizer.java
+++ common/src/main/java/com/cozary/nameless_trinkets/items/trinkets/Fertilizer.java
@@ -173,6 +173,7 @@ public class Fertilizer extends TrinketItem<Fertilizer.Stats> {
             tooltip.add(Component.translatable("tooltip.nameless_trinkets.fertilizer_lore").withStyle(ChatFormatting.AQUA, ChatFormatting.ITALIC));
             if (Screen.hasShiftDown()) {
                 tooltip.add(Component.translatable("tooltip.nameless_trinkets.fertilizer_1").withStyle(ChatFormatting.GOLD));
+                tooltip.add(Component.translatable("tooltip.nameless_trinkets.fertilizer_2").withStyle(ChatFormatting.GOLD));
             } else {
                 tooltip.add(Component.translatable("tooltip.nameless_trinkets.hold_shift"));
             }
