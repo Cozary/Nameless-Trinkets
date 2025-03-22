f704a36e850f2a8e4243c868fc6ae901fcb8a981
Cozary <ayrecio99@gmail.com>
2025-03-22 16:08:35 +0100

    Finished adding new trinkets to everything

--- common/src/main/java/com/cozary/nameless_trinkets/init/ModItems.java
+++ common/src/main/java/com/cozary/nameless_trinkets/init/ModItems.java
@@ -3,12 +3,9 @@ package com.cozary.nameless_trinkets.init;
 import com.cozary.nameless_trinkets.NamelessTrinkets;
 import com.cozary.nameless_trinkets.items.special.*;
 import com.cozary.nameless_trinkets.items.trinkets.*;
-import com.cozary.nameless_trinkets.utils.TrinketBundleContents;
 import com.google.common.collect.Sets;
-import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Rarity;
 
 import java.util.LinkedHashSet;
 import java.util.function.Supplier;
@@ -71,7 +68,7 @@ public class ModItems {
     public static final RegistryObject<Item> SHRINKING_VEIL = registerWithTab("shrinking_veil", ShrinkingVeil::new);
     public static final RegistryObject<Item> TITANS_MARK = registerWithTab("titans_mark", TitansMark::new);
     public static final RegistryObject<Item> WOUNDBEARER = registerWithTab("woundbearer", Woundbearer::new);
-    public static final RegistryObject<Item> DYINGSTAR = registerWithTab("dying_star", DyingStar::new);
+    public static final RegistryObject<Item> DYING_STAR = registerWithTab("dying_star", DyingStar::new);
     public static final RegistryObject<Item> RESONANT_HEART = registerWithTab("resonant_heart", ResonantHeart::new);
 
     public static final RegistryObject<Item> TRINKET_BUNDLE = registerWithTab("trinket_bundle", TrinketBundle::new);
