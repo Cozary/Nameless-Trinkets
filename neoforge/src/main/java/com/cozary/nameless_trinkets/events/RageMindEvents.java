c7739d6f45a069eb6c8484f05a1d100a07f8e112
Cozary <ayrecio99@gmail.com>
2025-04-01 13:04:46 +0200

    [Rage Mind] Not applying the "damageMultiplierPercentage" value correctly

--- neoforge/src/main/java/com/cozary/nameless_trinkets/events/RageMindEvents.java
+++ neoforge/src/main/java/com/cozary/nameless_trinkets/events/RageMindEvents.java
@@ -89,7 +89,7 @@ public class RageMindEvents {
                     }
 
                     if (event.getEntity().getClass() == classEntity) {
-                        event.setNewDamage(event.getOriginalDamage() * config.damageMultiplierPercentage);
+                        event.setNewDamage(event.getOriginalDamage() * (config.damageMultiplierPercentage/100));
                     }
                 }
 
