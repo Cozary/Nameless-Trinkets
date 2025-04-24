18dd06bbc21dbbf84aca3addff29300198fc3679
Cozary <ayrecio99@gmail.com>
2025-04-25 00:03:20 +0200

    [CreeperSense] fixed infinite explosion

--- common/src/main/java/com/cozary/nameless_trinkets/items/trinkets/CreeperSense.java
+++ common/src/main/java/com/cozary/nameless_trinkets/items/trinkets/CreeperSense.java
@@ -8,6 +8,7 @@ import io.wispforest.accessories.api.slot.SlotReference;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
@@ -17,6 +18,8 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.List;
 
@@ -54,12 +57,15 @@ public class CreeperSense extends TrinketItem<CreeperSense.Stats> {
             return;
         }
 
-        if (player.isCrouching()) {
+        BlockPos posAbove = player.blockPosition().above().above();
+        BlockState blockAbove = player.level().getBlockState(posAbove);
+
+        if (blockAbove.isAir() && player.isCrouching()) {
             player.level().explode(null, player.getX(), player.getY(), player.getZ(), config.explosionLevel, Level.ExplosionInteraction.NONE);
             player.setShiftKeyDown(false);
         }
-    }
 
+    }
 
     @Override
     public void appendHoverText(ItemStack stack, TooltipContext tooltipContext, List<Component> tooltip, TooltipFlag tooltipFlag) {
