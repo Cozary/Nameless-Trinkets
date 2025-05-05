a1848a83b392fa40e4d44e42634cc22b1240a795
Cozary <ayrecio99@gmail.com>
2025-05-05 15:09:33 +0200

    forge_config_api_removed

--- neoforge/src/main/java/com/cozary/nameless_trinkets/NamelessTrinketsNeoForge.java
+++ neoforge/src/main/java/com/cozary/nameless_trinkets/NamelessTrinketsNeoForge.java
@@ -1,15 +1,14 @@
 package com.cozary.nameless_trinkets;
 
 import com.cozary.nameless_trinkets.config.TrinketConfigs;
+import com.cozary.nameless_trinkets.config.common.CommonConfigManager;
 import com.cozary.nameless_trinkets.config.looTables.TrinketLootConfigsManager;
 import com.cozary.nameless_trinkets.init.ModTabs;
-import com.cozary.nameless_trinkets.config.general.ConfigurationHandler;
 import com.cozary.nameless_trinkets.utils.RemoveRendering;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 
 @Mod(NamelessTrinkets.MOD_ID)
@@ -22,14 +21,12 @@ public class NamelessTrinketsNeoForge {
         NamelessTrinkets.init();
 
         ModTabs.init(eventBus);
-
-        container.registerConfig(ModConfig.Type.COMMON, ConfigurationHandler.spec);
-
     }
 
     private void setup(final FMLCommonSetupEvent event) {
         TrinketLootConfigsManager.loadConfigs();
         TrinketConfigs.loadClass();
+        CommonConfigManager.loadConfig();
         RemoveRendering.noRenderingList();
     }
 }
