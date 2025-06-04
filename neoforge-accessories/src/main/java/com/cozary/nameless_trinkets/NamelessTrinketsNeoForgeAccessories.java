45389552bca891b3a92c02f200c79706c384975b
Cozary <ayrecio99@gmail.com>
2025-06-04 11:59:24 +0200

    Rename main class

--- neoforge-accessories/src/main/java/com/cozary/nameless_trinkets/NamelessTrinketsNeoForgeAccessories.java
+++ neoforge-accessories/src/main/java/com/cozary/nameless_trinkets/NamelessTrinketsNeoForgeAccessories.java
@@ -1,8 +1,10 @@
 package com.cozary.nameless_trinkets;
 
+import com.cozary.nameless_trinkets.config.TrinketConfigs;
 import com.cozary.nameless_trinkets.config.common.CommonConfigManager;
-import com.cozary.nameless_trinkets.init.ModItems;
+import com.cozary.nameless_trinkets.config.looTables.TrinketLootConfigsManager;
 import com.cozary.nameless_trinkets.init.ModTabs;
+import com.cozary.nameless_trinkets.util.RemoveRendering;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
@@ -10,21 +12,23 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 
 @Mod(NamelessTrinkets.MOD_ID)
-public class NamelessTrinketsNeoForge {
+public class NamelessTrinketsNeoForgeAccessories {
 
-    public NamelessTrinketsNeoForge(IEventBus eventBus, ModContainer container) {
+    public NamelessTrinketsNeoForgeAccessories(IEventBus eventBus, ModContainer container) {
 
-        eventBus.addListener(EventPriority.LOWEST, this::setup);
 
         NamelessTrinkets.init();
 
         ModTabs.init(eventBus);
+
+        eventBus.addListener(EventPriority.LOWEST, this::setup);
+
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-        //TrinketLootConfigsManager.loadConfigs();
-        //TrinketConfigs.loadClass();
+        TrinketLootConfigsManager.loadConfigs();
+        TrinketConfigs.loadClass();
         CommonConfigManager.loadConfig();
-        //RemoveRendering.noRenderingList();
+        RemoveRendering.noRenderingList();
     }
 }
--- neoforge-curios/src/main/java/com/cozary/nameless_trinkets/NamelessTrinketsNeoForgeCurios.java
+++ neoforge-curios/src/main/java/com/cozary/nameless_trinkets/NamelessTrinketsNeoForgeCurios.java
@@ -3,36 +3,28 @@ package com.cozary.nameless_trinkets;
 import com.cozary.nameless_trinkets.config.TrinketConfigs;
 import com.cozary.nameless_trinkets.config.common.CommonConfigManager;
 import com.cozary.nameless_trinkets.config.looTables.TrinketLootConfigsManager;
-import com.cozary.nameless_trinkets.init.ModItems;
 import com.cozary.nameless_trinkets.init.ModTabs;
-import com.cozary.nameless_trinkets.items.trinkets.SpeedForce;
-import com.cozary.nameless_trinkets.util.RemoveRendering;
-import io.wispforest.accessories.api.AccessoriesAPI;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 
 @Mod(NamelessTrinkets.MOD_ID)
-public class NamelessTrinketsNeoForge {
+public class NamelessTrinketsNeoForgeCurios {
 
-    public NamelessTrinketsNeoForge(IEventBus eventBus, ModContainer container) {
+    public NamelessTrinketsNeoForgeCurios(IEventBus eventBus, ModContainer container) {
 
+        eventBus.addListener(EventPriority.LOWEST, this::setup);
 
         NamelessTrinkets.init();
 
         ModTabs.init(eventBus);
-
-        eventBus.addListener(EventPriority.LOWEST, this::setup);
-
     }
 
     private void setup(final FMLCommonSetupEvent event) {
         TrinketLootConfigsManager.loadConfigs();
         TrinketConfigs.loadClass();
         CommonConfigManager.loadConfig();
-        RemoveRendering.noRenderingList();
     }
 }
