61bac0ca6bafe34bccd222fbea5aa89da2de0681
StoffiPoff <136234499+StoffiPoff@users.noreply.github.com>
2025-07-21 20:38:57 +0200

    Update MixinBoat.java
    Fixing crash related to entering chestboats

--- neoforge-curios/src/main/java/com/cozary/nameless_trinkets/mixin/MixinBoat.java
+++ neoforge-curios/src/main/java/com/cozary/nameless_trinkets/mixin/MixinBoat.java
@@ -5,7 +5,6 @@ import com.cozary.nameless_trinkets.mixinHandler.MixinBoatHandler;
 import com.cozary.nameless_trinkets.util.TrinketUtils;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.vehicle.AbstractBoat;
-import net.minecraft.world.entity.vehicle.Boat;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,16 +13,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(AbstractBoat.class)
 public class MixinBoat {
 
-    @Inject(at = @At(value = "HEAD"), method = "controlBoat")
+    @Inject(at = @At("HEAD"), method = "controlBoat")
     protected void controlSpeed(CallbackInfo ci) {
-        if (((Boat) (Object) this).getFirstPassenger() instanceof Player player) {
+        AbstractBoat boat = (AbstractBoat) (Object) this;
 
+        if (boat.getFirstPassenger() instanceof Player player) {
             var stack = TrinketUtils.getEquippedTrinket(player, ModItems.ELECTRIC_PADDLE.get());
 
             if (!stack.isEmpty())
                 return;
 
-            MixinBoatHandler.applyBoostedBoatControl((Boat) (Object) this, player);
+            MixinBoatHandler.applyBoostedBoatControl(boat, player);
         }
     }
 }
+
