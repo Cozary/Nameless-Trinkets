8f2ad8cc3823aa599a4c079698de3297cce02307
StoffiPoff <136234499+StoffiPoff@users.noreply.github.com>
2025-07-22 08:37:21 +0200

    Update MixinBoat.java

--- neoforge-curios/src/main/java/com/cozary/nameless_trinkets/mixin/MixinBoat.java
+++ neoforge-curios/src/main/java/com/cozary/nameless_trinkets/mixin/MixinBoat.java
@@ -5,6 +5,8 @@ import com.cozary.nameless_trinkets.mixinHandler.MixinBoatHandler;
 import com.cozary.nameless_trinkets.util.TrinketUtils;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.vehicle.AbstractBoat;
+import net.minecraft.world.entity.vehicle.Boat;
+import net.minecraft.world.entity.vehicle.ChestBoat;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,18 +15,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(AbstractBoat.class)
 public class MixinBoat {
 
-    @Inject(at = @At("HEAD"), method = "controlBoat")
+    @Inject(at = @At("TAIL"), method = "controlBoat")
     protected void controlSpeed(CallbackInfo ci) {
-        AbstractBoat boat = (AbstractBoat) (Object) this;
+        AbstractBoat boat = (AbstractBoat)(Object) this;
 
         if (boat.getFirstPassenger() instanceof Player player) {
             var stack = TrinketUtils.getEquippedTrinket(player, ModItems.ELECTRIC_PADDLE.get());
 
-            if (!stack.isEmpty())
+            if (stack.isEmpty())
                 return;
 
-            MixinBoatHandler.applyBoostedBoatControl(boat, player);
+            if (boat instanceof Boat vanillaBoat) {
+                MixinBoatHandler.applyBoostedBoatControl(vanillaBoat, player);
+            } else if (boat instanceof ChestBoat chestBoat) {
+                MixinBoatHandler.applyBoostedBoatControl(chestBoat, player);
+            } else {
+                MixinBoatHandler.applyBoostedBoatControl(boat, player);
+            }
         }
     }
 }
-
