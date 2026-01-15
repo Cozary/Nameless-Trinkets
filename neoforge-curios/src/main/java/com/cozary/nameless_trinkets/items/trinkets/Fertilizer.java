4ec4a945459d774a90e6457107d184fd93a71467
Cozary <ayrecio99@gmail.com>
2026-01-15 17:19:31 +0100

    [Fertilizer] Refactored logic and added underwater surface safety #39 (@JahsonGA)

--- neoforge-curios/src/main/java/com/cozary/nameless_trinkets/items/trinkets/Fertilizer.java
+++ neoforge-curios/src/main/java/com/cozary/nameless_trinkets/items/trinkets/Fertilizer.java
@@ -1,13 +1,7 @@
 package com.cozary.nameless_trinkets.items.trinkets;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
@@ -25,38 +19,7 @@ public class Fertilizer extends FertilizerBase implements ICurioItem {
 
     @Override
     public void curioTick(SlotContext slotContext, ItemStack stack) {
-        Stats config = FertilizerBase.INSTANCE.getTrinketConfig();
-
-        LivingEntity entity = slotContext.entity();
-        Level level = entity.level();
-
-        if (level.isClientSide) {
-            return;
-        }
-
-        RandomSource random = level.getRandom();
-        BlockPos playerPos = entity.blockPosition();
-        BlockPos targetPos = playerPos.offset(
-                random.nextInt(5) - 3,
-                random.nextInt(3) - 2,
-                random.nextInt(5) - 3
-        );
-
-        int effectInterval = config.effectIntervalInTicks;
-        if (entity.tickCount % effectInterval == 0) {
-            BlockState targetState = level.getBlockState(targetPos);
-            BlockState stateBelow = level.getBlockState(playerPos.below());
-
-            if (stateBelow.is(Blocks.GRASS_BLOCK)) {
-                if (applyBonemeal(level, targetPos)) {
-                    spawnGrowthParticles(level, targetPos, 3);
-                }
-            } else if (targetState.is(Blocks.WATER)) {
-                if (growWaterPlant(level, targetPos, null)) {
-                    spawnGrowthParticles(level, targetPos, 3);
-                }
-            }
-        }
+        commonTick(stack, slotContext.entity());
     }
 
 }
