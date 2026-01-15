d1529e9b0f027f1af899f16e7d69571f73eebb2b
Cozary <ayrecio99@gmail.com>
2026-01-15 13:39:39 +0100

    Fix data persistence issues for [Woundbearer], [Sigil of Baphomet], and [Rage Mind]

--- common/src/main/java/com/cozary/nameless_trinkets/events/WoundbearerHandler.java
+++ common/src/main/java/com/cozary/nameless_trinkets/events/WoundbearerHandler.java
@@ -3,11 +3,11 @@ package com.cozary.nameless_trinkets.events;
 import com.cozary.nameless_trinkets.init.ModDataComponents;
 import com.cozary.nameless_trinkets.items.trinkets.WoundbearerBase;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 
 public class WoundbearerHandler {
 
-    public static void savePlayerDamageIncrement(Player player, float damageAmount, Item stack) {
+    public static void savePlayerDamageIncrement(Player player, float damageAmount, ItemStack stack) {
         WoundbearerBase.Stats config = WoundbearerBase.INSTANCE.getTrinketConfig();
         if (!config.isEnable)
             return;
@@ -16,14 +16,14 @@ public class WoundbearerHandler {
         if (!player.level().isClientSide) {
             float damageIncrement = damageAmount * (config.damageConversionPercentage / 100);
 
-            float currentDamage = stack.getDefaultInstance().getOrDefault(ModDataComponents.WOUNDBEARER_DAMAGE.get(), 0f);
+            float currentDamage = stack.getOrDefault(ModDataComponents.WOUNDBEARER_DAMAGE.get(), 0f);
 
             float newDamage = currentDamage + damageIncrement;
             if (Float.isInfinite(newDamage) || newDamage > Float.MAX_VALUE) {
                 newDamage = Float.MAX_VALUE;
             }
 
-            stack.getDefaultInstance().set(ModDataComponents.WOUNDBEARER_DAMAGE.get(), newDamage);
+            stack.set(ModDataComponents.WOUNDBEARER_DAMAGE.get(), newDamage);
         }
     }
 }
